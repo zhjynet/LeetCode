38c6006bac0bacf97f24121c89608c2580c4f71f
张靖宇 <253841389@qq.com>
2020-08-12 16:40:51 +0800

    1、两数之和
    Signed-off-by: 张靖宇 <253841389@qq.com>

---
+++ src/Hash/q1/Solution1.java
@@ -0,0 +1,35 @@
+package Hash.q1;
+
+/**
+ * @author zhangjingyu
+ * @version V1.0
+ * @Title: Solution1
+ * @Package Hash.q1
+ * @Description: TODO
+ * @date 2020/8/12 16:19
+ */
+public class Solution1 {
+    /**
+     * @Title twoSum
+     * @Description //
+     * @author zhangjingyu
+     * @date 16:36 2020/8/12
+     * @param nums
+     * @param target
+     * @return int[]
+     * @version V1.0.0
+    */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
