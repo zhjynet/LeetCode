36cf34d34e8d17f463ff330a71e1d2b732fff3fb
张靖宇 <253841389@qq.com>
2020-08-18 18:42:47 +0800

    25题：仍有问题。

---
+++ src/linkedlist/q25/Soluton1.java
@@ -0,0 +1,58 @@
+package linkedlist.q25;
+
+/**
+ * @author zhangjingyu
+ * @version V1.0.0
+ * @Title: Soluton1
+ * @Package linkedlist.q25
+ * @Description: TODO
+ * @date 2020/8/17 09:59
+ */
+public class Soluton1 {
+    /**
+     * @param head 传入头结点
+     * @param k    正整数
+     * @return linkedlist.q25.ListNode
+     * @Title reverseKGroup
+     * @Description //TODO
+     * @author zhangjingyu
+     * @date 2020/8/17 10:00
+     * @version V1.0.0
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for (int i = 0; i < k & end != null; i++){
+                end = end.next;
+            }
+            if (end == null){
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode first = head;
+        ListNode result = null;
+        ListNode temp;
+        while (first != null) {
+            temp = first.next;
+            first.next = result;
+            result = first;
+            first = temp;
+        }
+        return result;
+    }
+}
