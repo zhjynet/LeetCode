01420fff226e6fc20b945a304723c5339150daaa
zhangjingyu <zhangjy@zjrc.com>
2021-01-06 22:24:59 +0800

    剑指 Offer 03. 数组中重复的数字

--- src/cn/zhjynet/lcof/offer03/Solution1.java
+++ src/cn/zhjynet/lcof/offer03/Solution1.java
@@ -1,5 +1,8 @@
 package cn.zhjynet.lcof.offer03;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author zhangjingyu
  * @version V1.0
@@ -9,14 +12,21 @@ package cn.zhjynet.lcof.offer03;
  * @date 2021/1/6 19:12
  */
 public class Solution1 {
-    /*
-     * @Author chengpunan
-     * @Description //TODO
-     * @Date 19:41 2021/1/6
-     * @Param [nums]
+    /**
      * @return int
+     * @Author zhangjingyu
+     * @Description //TODO
+     * @Date 20:45 2021/1/6
+     * @Param [nums]
      **/
     public int findRepeatNumber(int[] nums) {
-
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+        return -1;
     }
 }
